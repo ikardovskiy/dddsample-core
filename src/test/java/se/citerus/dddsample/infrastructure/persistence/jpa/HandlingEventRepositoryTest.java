15d5c586b8454daf6545bf33513bb2478db734b2
orende <1337decker@gmail.com>
2022-10-10 19:59:35 +0200

    Fix errors in JPA mapping annotation usage, implement repos, fix tests
    This commit uses a different approach where the JPA-annotated classes are spread throughout the codebase instead of trying to keep them separate using converters.

--- src/test/java/se/citerus/dddsample/infrastructure/persistence/jpa/HandlingEventRepositoryTest.java
+++ src/test/java/se/citerus/dddsample/infrastructure/persistence/jpa/HandlingEventRepositoryTest.java
@@ -1,30 +1,12 @@
 package se.citerus.dddsample.infrastructure.persistence.jpa;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.lang.reflect.Field;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import javax.sql.DataSource;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
-
-import se.citerus.dddsample.application.util.SampleDataGenerator;
 import se.citerus.dddsample.domain.model.cargo.Cargo;
 import se.citerus.dddsample.domain.model.cargo.CargoRepository;
 import se.citerus.dddsample.domain.model.cargo.TrackingId;
@@ -34,8 +16,15 @@ import se.citerus.dddsample.domain.model.location.Location;
 import se.citerus.dddsample.domain.model.location.LocationRepository;
 import se.citerus.dddsample.domain.model.location.UnLocode;
 
+import javax.persistence.EntityManager;
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@TestPropertySource(locations = {"/application.properties", "/config/application.properties"})
+@DataJpaTest
+@ContextConfiguration(classes = TestRepositoryConfig.class)
 @Transactional
 public class HandlingEventRepositoryTest {
 
@@ -49,27 +38,13 @@ public class HandlingEventRepositoryTest {
     LocationRepository locationRepository;
 
     @Autowired
-    SessionFactory sessionFactory;
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    private PlatformTransactionManager transactionManager;
-
-    private JdbcTemplate jdbcTemplate;
-
-    @Before
-    public void setup() {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-        SampleDataGenerator.loadSampleData(jdbcTemplate, new TransactionTemplate(transactionManager));
-    }
+    EntityManager entityManager;
 
     @Test
     public void testSave() {
         Location location = locationRepository.find(new UnLocode("SESTO"));
 
-        Cargo cargo = cargoRepository.find(new TrackingId("XYZ"));
+        Cargo cargo = cargoRepository.find(new TrackingId("ABC123"));
         Date completionTime = new Date(10);
         Date registrationTime = new Date(20);
         HandlingEvent event = new HandlingEvent(cargo, completionTime, registrationTime, HandlingEvent.Type.CLAIM, location);
@@ -78,41 +53,26 @@ public class HandlingEventRepositoryTest {
 
         flush();
 
-        Map<String, Object> result = jdbcTemplate.queryForMap("select * from HandlingEvent where id = ?", getLongId(event));
+        HandlingEvent result = entityManager.createQuery(String.format("select he from HandlingEvent he where he.id = %d", event.id), HandlingEvent.class).getSingleResult();
 
-        assertThat(result.get("CARGO_ID")).isEqualTo(1L);
-        Date completionDate = new Date(((Timestamp) result.get("COMPLETIONTIME")).getTime()); // equals call is not symmetric between java.sql.Timestamp and java.util.Date, so we should convert Timestamp Date
+        assertThat(result.cargo.id).isEqualTo(cargo.id);
+        Date completionDate = result.completionTime;
         assertThat(completionDate).isEqualTo(new Date(10));
-        Date registrationDate = new Date(((Timestamp) result.get("REGISTRATIONTIME")).getTime()); // equals call is not symmetric between java.sql.Timestamp and java.util.Date, so we should convert Timestamp Date
+        Date registrationDate = result.registrationTime;
         assertThat(registrationDate).isEqualTo(new Date(20));
-        assertThat(result.get("TYPE")).isEqualTo("CLAIM");
+        assertThat(result.type).isEqualTo(HandlingEvent.Type.CLAIM);
         // TODO: the rest of the columns
     }
 
     private void flush() {
-        sessionFactory.getCurrentSession().flush();
-    }
-
-    private Long getLongId(Object o) {
-        final Session session = sessionFactory.getCurrentSession();
-        if (session.contains(o)) {
-            return (Long) session.getIdentifier(o);
-        } else {
-            try {
-                Field id = o.getClass().getDeclaredField("id");
-                id.setAccessible(true);
-                return (Long) id.get(o);
-            } catch (Exception e) {
-                throw new RuntimeException();
-            }
-        }
+        entityManager.flush();
     }
 
     @Test
     public void testFindEventsForCargo() {
-        TrackingId trackingId = new TrackingId("XYZ");
+        TrackingId trackingId = new TrackingId("ABC123");
         List<HandlingEvent> handlingEvents = handlingEventRepository.lookupHandlingHistoryOfCargo(trackingId).distinctEventsByCompletionTime();
-        assertThat(handlingEvents).hasSize(12);
+        assertThat(handlingEvents).hasSize(3);
     }
 
 }
